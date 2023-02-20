f808e13072f8cda24c7a181dcf6ab726ce682082
mostafaehab96 <mostafa96ehab@gmail.com>
2023-02-20 04:42:15 +0000

    The premature code of main function (has a segmentation fault error)

--- shell.c
+++ shell.c
@@ -1,28 +1,56 @@
 #include <stdio.h>
-#include <unistdlib.h>
-#include <stlib.h>
-
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+#include <sys/wait.h>
+#include <string.h>
 
 
 int main(int argc, char **argv, char **env)
 {
+	char *input;
 	char *command;
+	list_t *path_list;
+	char *path;
 	size_t len;
 	ssize_t read = 1;
 	int status;
 	pid_t pid;
-	int error;
+	char *token;
+	char **args;
+
+
+	path = getenv("PATH");
+	path_list = create(path);
 
 	while(read)
 	{
 		write(1, "$ ", 2);
-		command = getline(&command, &len, stdin);
-
-
-		if (command == NULL || read = -1)
+		read = getline(&input, &len, stdin);
+		/* if EOF */
+		if (input == NULL || read == -1)
 			break;
+		/*
+		token = strtok(input, "\n");
+		while (token != NULL)
+		{
+			args = get_command(token);
+			if ((command = search(args[0], path_list)) != NULL)
+			{
+				pid = fork();
+				if (pid == 0)
+					execve(command, args, env);
+				else
+					wait(&status);
+			}
+			else
+				perror("Command Not Found");
+		}
+		*/
 
 	}
+
+	/*free_list(path_list);*/
 
 	write(1, "exit\n", 5);
 
